e4dfb98c0d677ae0afb9c07f96e022e9bc66b268
TzuYuOu <a1225johnny@gmail.com>
2020-04-09 11:16:10 +0800

    Add constraint 6,7

--- question3.py
+++ question3.py
@@ -1,9 +1,11 @@
 from pulp import *
 import math
 
+# constant
 p_i = [16,16,16,16,9,9,9,9,9,18,18,18]
 q_i = [8,8,8,8,9,9,9,9,9,3,3,3]
-
+X_UPPER_BOUND = 163
+Y_UPPER_BOUND = 163
 # Define decision variables
 x_i = {}
 y_i = {}
@@ -19,7 +21,14 @@ y = LpVariable("y",0,None,LpInteger)
 # Define problem
 prob =  LpProblem("MinimizeRectangleArea",LpMinimize)
 prob += x+y , "Area"
-
+# constraint 6
+prob += X_UPPER_BOUND >= x
+for i in range(1,13):
+    prob += x >= x_i["x_{}".format(i)] + (p_i[i-1]*s_i["s_{}".format(i)] + q_i[i-1](1-s_i["s_{}".format(i)]))/2
+# constraint 7
+prob += Y_UPPER_BOUND >= y
+for i in range(1,13):
+    prob += y >= y_i["y_{}".format(i)] + (p_i[i-1]*(1-s_i["s_{}".format(i)]) + q_i[i-1]*s_i["s_{}".format(i)])/2
 # constraint 8,9
 for i in range(1,13):
     prob += x_i["x_{}".format(i)] - (p_i[i-1]*s_i["s_{}".format(i)] + q_i[i-1](1-s_i["s_{}".format(i)]))/2 >= 0
